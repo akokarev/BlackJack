2c4648f03f0187b21a3c0e0a9b2ea5f3ce9b0835
Anton Kokarev <net-files@yandex.ru>
2024-02-08 08:39:33 +0300

    Cards color

--- card.rb
+++ card.rb
@@ -1,10 +1,16 @@
 class Card
-	SUITS = ["\0330;30♣\033", "\0330;0♠\033", "\0330;31♥\033", "\0330;31♦\033"]
-	CARDS = [2,3,4,5,6,7,8,9,10,"B","D","K","T"]
+	SUITS = ['♣', '♠', '♥', '♦']
+	VALUES = [2,3,4,5,6,7,8,9,10,"B","D","K","T"]
 
 	attr_reader :suit, :value
 	def initialize(suit, value)
 		@suit = suit
-		@value - value
+		@value = value
+	end
+
+	def to_s
+		return "\033[47;30m#{value}#{suit}\033[0m" if ['♣', '♠'].include?(suit)
+		return "\033[47;31m#{value}#{suit}\033[0m" if ['♥', '♦'].include?(suit)
+		"#{value}#{suit}"
 	end
 end
