12713d5e4c9b6b164f2918d330d3cecf43503a9d
Anton Kokarev <net-files@ya.ru>
2024-02-14 15:33:21 +0300

    release

--- dealer.rb
+++ dealer.rb
@@ -7,4 +7,9 @@ class Dealer < Player
 	def to_s
 		"#{name} ($#{cash}) #{cards.map{"\033[47;34m * \033[0m"}.join(' ')}"
 	end
+
+	def turn(bank)
+		self.take_card(bank.cards.pop) if (self.points < 17) && (self.cards.count == 2)
+	end
+
 end
--- main.rb
+++ main.rb
@@ -10,43 +10,84 @@ print 'Player name: '
 player1 = Player.new(gets.chomp, 100)
 
 
-bank.lets_play
-dealer.lets_play(bank)
-player1.lets_play(bank)
+while (player1.cash > 0) && (dealer.cash > 0)
 
-puts "Ставки сделаны, игра начинается!"
-while true
-  puts bank
-  puts dealer
-  puts player1
-  puts '1 - Пропустить ход'
-  puts '2 - Добавить карту' if player1.cards.count==2
-  puts '3 - Открыть карты'
+  bank.lets_play
+  dealer.lets_play(bank)
+  player1.lets_play(bank)
 
-  case choice = gets.chomp.to_i
-  when 1
-  	dealer.take_card(bank.cards.pop) if (dealer.points < 17) && (dealer.cards.count == 2)
-    break if dealer.points > 21
-  when 2
-  	if player1.cards.count == 2
-  		player1.take_card(bank.cards.pop)
-      break if player1.points > 21
-  	else
-  		puts 'Нельзя добавить карту'
-  	end
-  when 3
-    break  	
-  else
-  	puts 'Введите цифру соответвстующую вашему выбору'
+  puts
+  puts "Ставки сделаны, игра начинается!"
+  while true
+    puts bank
+    puts dealer
+    puts player1
+    puts '1 - Пропустить ход'
+    puts '2 - Добавить карту' if player1.cards.count==2
+    puts '3 - Открыть карты'
+
+    print '> '
+    case choice = gets.chomp.to_i
+    when 1
+      dealer.turn(bank)
+      break if dealer.points > 21
+    when 2
+      if player1.cards.count == 2
+        player1.take_card(bank.cards.pop)
+        break if player1.points > 21
+        dealer.turn(bank)
+        break if dealer.points > 21
+      else
+        puts 'Нельзя добавить карту'
+      end
+    when 3
+      break    
+    else
+      puts 'Введите цифру соответвстующую вашему выбору'
+    end
+
+    break if player1.cards.count == 3
   end
 
-  break if player1.cards.count == 3
+  puts "Игра окончена, подсчёт очков:"
+  puts dealer.show_cards
+  puts player1.show_cards
+  if (dealer.points > 21) && (player1.points > 21)
+    puts "Перебрали все/Ничья"
+    dealer.take_money(bank, bank.cash / 2)
+    player1.take_money(bank)
+  elsif dealer.points > 21
+    puts "#{dealer.name} перебрал"
+    player1.take_money(bank)
+  elsif player1.points > 21
+    puts "#{player1.name} перебрал, победа #{dealer.name}"
+    dealer.take_money(bank)
+  elsif dealer.points == player1.points
+    puts "Ничья по очкам"
+    dealer.take_money(bank, bank.cash / 2)
+    player1.take_money(bank)
+  elsif dealer.points > player1.points
+    puts "#{dealer.name} выиграл по очкам"
+    dealer.take_money(bank)
+  else
+    puts "#{player1.name} выиграл по очкам"
+    player1.take_money(bank)
+  end
+
+  puts
+  puts "#{player1.name}, на Вашем счету $#{player1.cash}"
+
+  while true
+  	print 'Сыграем ещё? (Y/n) > '
+  	choice = gets.chomp.upcase
+  	break if ['Y','N',''].include?(choice)
+  end
+  break if choice == 'N'
 end
 
-puts "Игра окончена, подсчёт очков:"
-puts dealer.show_cards
-puts player1.show_cards
-if (dealer.points > 21) && (player1.points > 21)
-  puts "Перебрали все/Ничья"
-end
-
+puts "По итогам игры на Вашем счету $#{player1.cash}"
+puts "Вы банкрот!" if player1.cash == 0
+puts "Вам удалось подзаработать!" if player1.cash > dealer.cash
+puts "Поздравляем, Вы обанкротили диллера!" if dealer.cash == 0
+puts "Все остались при своих." if player1.cash ==  dealer.cash
+puts "Повезет в следующий раз." if player1.cash < dealer.cash
